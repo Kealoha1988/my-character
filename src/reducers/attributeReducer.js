5fd6449624beb3891d6c6f623c1f5e7e820f6c60
Kealoha1988 <kealohaabe1988@gmail.com>
2021-04-12 23:58:52 -0400

    added case LOADING, SET_USER, & MAKE_CHARACTER

--- src/reducers/attributeReducer.js
+++ src/reducers/attributeReducer.js
@@ -10,7 +10,9 @@ const initialState = {
     mouthIndex: "",
     shirtIndex: "",
     pantsIndex: "",
-    shoesIndex: "",j
+    shoesIndex: "",
+    id: "",
+    user_id: ""
   },
   loading: true
 }
@@ -20,11 +22,38 @@ const initialState = {
 const attributeReducer = (state = initialState, action) => {
   switch(action.type) {
     case "LOADING":
-      return{
+        return{
       ...state,
       loading: true
       }
-     
+      case "SET_USER":
+        return{
+         ...state,
+         user: {
+           name: this.state.name
+           id: this.state.id
+         },
+         loading: false
+        }
+      case "MAKE_CHARACTER":
+        return{
+          ...state,
+          user: {
+            name: this.state.name,
+            id: this.state.id
+          },
+          chararcter: {
+            name: this.state.name, 
+            hairIndex: this.state.hairIndex,
+            eyesIndex: this.state.eyeIndex,
+            mouthIndex: this.state.mouthIndex,
+            shirtIndex: this.state.shirtIndex,
+            pantsIndex: this.state.pantsIndex,
+            shoesIndex: this.state.shoesIndex,
+            id: this.state.id,
+            user_id: this.state.user_id
+          },
+          loading: false
       }
 
     default:
