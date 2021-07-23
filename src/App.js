21a89fc65e15f00319476d9a21e17130cd0dae39
Kealoha1988 <kealohaabe1988@gmail.com>
2021-07-22 23:27:16 -0400

    added MessageForm

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Nav from './Nav'
 import { generatePath } from "react-router"
 import { connect } from 'react-redux'
 import{ getUserState } from './actions'
-import Messages from './components/user/Messages'
+import Messages from '/Users/kealohaabe/Development/code/Projects/my-character/src/messages/Messages.js'
 
 
 
