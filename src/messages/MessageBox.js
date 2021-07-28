8e696a6a661215c6320bd5fe4cbea0a2bed44aaa
Kealoha1988 <kealohaabe1988@gmail.com>
2021-07-28 01:15:45 -0400

    created message area

--- src/messages/MessageBox.js
+++ src/messages/MessageBox.js
@@ -3,7 +3,8 @@ import React from 'react'
 export default function MessageBox() {
   return (
     <div>
-      
+      <input type="text" className="textBoxes"></input>
+      <button className="button">send</button>
     </div>
   )
 }
--- src/messages/Messages.js
+++ src/messages/Messages.js
@@ -1,12 +1,21 @@
 import React from 'react'
+import MessageBox from './MessageBox'
 
 
 export default function Messages(props) {
+
+
+
+
   return (
     <div>
       <center>
       <h4 className="selectorText" id="h4">messages</h4>
-      <h3>{props.currentUser}</h3>
+      <div className="singleUser-container">
+      <h3 className="selectorText">Creator: {props.currentUser} says: there will be messages here</h3>
+      </div>
+      <br/>
+      <MessageBox/>
       </center>
     </div>
   )
