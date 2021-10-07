261cf3328e52237222b059c563ba0e568d23c6e4
Kealoha1988 <kealohaabe1988@gmail.com>
2021-10-07 12:39:52 -0400

    created music state

--- src/Nav.js
+++ src/Nav.js
@@ -4,6 +4,7 @@ import { useState } from 'react'
 
 export default function Nav(props) {
 
+  const [music, setMusic] = useState(true)
 
 
   const routerTrick = () => `/user/${props.cool}/characters`
