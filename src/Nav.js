8a345b29bce102868110ba5379c26ba53d13ab8a
Kealoha1988 <kealohaabe1988@gmail.com>
2021-10-07 12:39:00 -0400

    imported useState to nav

--- src/Nav.js
+++ src/Nav.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useState } from 'react'
 
 export default function Nav(props) {
+
 
 
   const routerTrick = () => `/user/${props.cool}/characters`
